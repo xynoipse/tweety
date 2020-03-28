f6d60c614d94476b619c204070b429708574d61a
Jason <28811675+xynoipsec@users.noreply.github.com>
2020-03-28 16:53:29 +0800

    Add timeline static design

---
+++ resources/views/_publish-tweet-panel.blade.php
@@ -0,0 +1,10 @@
+<div class="border border-blue-400 rounded-lg px-8 py-6 mb-8">
+    <form action="">
+        <textarea class="w-full" name="body" placeholder="What's up?"></textarea>
+        <hr class="my-4">
+        <div class="flex justify-between">
+            <img class="rounded-full mr-2" src="https://i.pravatar.cc/40" alt="">
+            <button class="bg-blue-500 rounded-lg shadow p-2 text-white" type="submit">Tweet!</button>
+        </div>
+    </form>
+</div>
---
+++ resources/views/_tweet.blade.php
@@ -0,0 +1,9 @@
+<div class="flex p-4 border-b border-b-gray-400">
+    <div class="mr-2 flex-shrink-0">
+        <img class="rounded-full mr-2" src="https://i.pravatar.cc/50" alt="">
+    </div>
+    <div>
+        <h5 class="font-bold mb-2">John Doe</h5>
+        <p class="text-sm">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam fuga corrupti hic doloribus iure, fugiat voluptatum minus ab expedita enim. Quae officia voluptate cumque accusamus eligendi illo! Quasi animi, distinctio aliquid aliquam dolore laboriosam dignissimos ipsum, placeat nisi nobis minima.</p>
+    </div>
+</div>
