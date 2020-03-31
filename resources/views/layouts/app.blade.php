e3f75b4d80d9a7a3f021907e85ca3885e879591c
Jason <28811675+xynoipsec@users.noreply.github.com>
2020-03-31 17:31:09 +0800

    Update layout views

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,7 +29,17 @@
 
         <section class="px-8">
             <main class="container mx-auto">
-                @yield('content')
+                <div class="lg:flex lg:justify-center">
+                    <div class="lg:w-32">
+                        @include('_sidebar-links')
+                    </div>
+                    <div class="lg:flex-1 lg:mx-10" style="max-width: 700px">
+                        @yield('content')
+                    </div>
+                    <div class="lg:w-1/6">
+                        @include('_following-list')
+                    </div>
+                </div>
             </main>
         </section>
     </div>
