2e503bd191f885aee1e20b3632d12cb2087d0043
Jason <28811675+xynoipsec@users.noreply.github.com>
2020-04-14 13:31:25 +0800

    Add username and avatar fields on users table

--- app/User.php
+++ app/User.php
@@ -17,7 +17,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'username', 'name', 'email', 'password',
     ];
 
     /**
@@ -60,6 +60,6 @@ class User extends Authenticatable
 
     public function getRouteKeyName()
     {
-        return 'name';
+        return 'username';
     }
 }
